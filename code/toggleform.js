1407a5c2b4b760036d45b32947c8e408f00eaa90
zimlewis <136766037+zimlewis@users.noreply.github.com>
2023-06-18 17:45:57 +0700

    Update toggleform.js

--- code/toggleform.js
+++ code/toggleform.js
@@ -1,10 +1,22 @@
 function openAccountPage(){
     var f = document.getElementById("login-form");
     f.style.display = "block";
+    enableScrolling();
     
 }
 function closeAccountPage(){
     var f = document.getElementById("login-form");
     f.style.display = "none";
+    disableScrolling();
     
+}
+
+function disableScrolling(){
+    var x=window.scrollX;
+    var y=window.scrollY;
+    window.onscroll=function(){window.scrollTo(x, y);};
+}
+
+function enableScrolling(){
+    window.onscroll=function(){};
 }
