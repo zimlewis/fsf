544e832c0c6d25f739c8a46bd35a00a07f68f9a2
zimlewis <z1ml3w1s123@gmail.com>
2023-06-20 16:09:43 +0700

    20/06/23 update

---
+++ code/toggleform.js
@@ -0,0 +1,12 @@
+function openAccountPage(){
+    var f = document.getElementById("login-form");
+    f.style.display = "block";
+    
+}
+function closeAccountPage(){
+    var f = document.getElementById("login-form");
+    f.style.display = "none";
+    
+    
+}
+
